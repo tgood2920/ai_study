047496c8205d40922cd36fd3dce58b6a3d33937f
tgood2920 <tglaon@gmail.com>
2025-12-20 02:05:52 +0900

    엑셀 시트 구조 변경13

--- excel_gen.py
+++ excel_gen.py
@@ -64,34 +64,25 @@ def create_basic_info_excel(data, project_alias):
         curr_row += 1
         
         basic_data = data.get('basic', {})
-        
-        # 예: '공식사업명'은 너무 기니까 0~3열을 모두 병합해서 한 줄로 표시
+
         if "공식사업명" in basic_data:
             worksheet.write(curr_row, 0, "공식사업명", head_fmt)
-            # 1열부터 3열까지 병합하여 내용을 채움
             worksheet.merge_range(curr_row, 1, curr_row, 3, str(basic_data["공식사업명"]), cell_fmt)
             curr_row += 1
-            # 처리한 항목은 리스트에서 제외하기 위해 pop 사용 가능
             basic_data.pop("공식사업명")
 
-        # 나머지 항목들은 기존처럼 2열씩 배치
         items = list(basic_data.items())
         for i in range(0, len(items), 2):
-            # 첫 번째 쌍 (A, B열)
             worksheet.write(curr_row, 0, items[i][0], head_fmt)
             worksheet.write(curr_row, 1, str(items[i][1]) if items[i][1] else "", cell_fmt)
-            
-            # 두 번째 쌍 (C, D열)
+
             if i + 1 < len(items):
                 worksheet.write(curr_row, 2, items[i+1][0], head_fmt)
                 worksheet.write(curr_row, 3, str(items[i+1][1]) if items[i+1][1] else "", cell_fmt)
             else:
-                # 홀수개일 때 나머지 칸도 병합하거나 빈칸 처리
                 worksheet.write(curr_row, 2, "", head_fmt)
                 worksheet.write(curr_row, 3, "", cell_fmt)
             curr_row += 1
-
-
 
         # 2. 사업담당자 섹션
         worksheet.merge_range(curr_row, 0, curr_row, 3, " # 사업담당자", title_fmt)
@@ -109,39 +100,41 @@ def create_basic_info_excel(data, project_alias):
         curr_row += 1
 
         # 3. 주요 이슈 섹션
-        worksheet.merge_range(curr_row, 0, curr_row, 2, " # 주요 이슈(특이사항)", title_fmt)
+        worksheet.merge_range(curr_row, 0, curr_row, 3, " # 주요 이슈(특이사항)", title_fmt)
         curr_row += 1
-        headers = ["구분", "주요사항", "비고"]
-        for col, h in enumerate(headers):
-            worksheet.write(curr_row, col, h, head_fmt)
+
+        # 헤더 병합 (구분:0, 주요사항:1~2합병, 비고:3)
+        worksheet.write(curr_row, 0, "구분", head_fmt)
+        worksheet.merge_range(curr_row, 1, curr_row, 2, "주요사항", head_fmt)
+        worksheet.write(curr_row, 3, "비고", head_fmt)
         curr_row += 1
+
         for i in data.get('issues', []):
             worksheet.write(curr_row, 0, i.get("구분", ""), cell_fmt)
-            
-            if "주요사항" in basic_data:
-                worksheet.merge_range(curr_row, 1, curr_row, 3, str(basic_data["주요사항"]), cell_fmt)
-                worksheet.write(curr_row, 1, i.get("주요사항", ""), cell_fmt)
-                basic_data.pop("주요사항")
-
+            # 1~2열 병합하여 주요사항 기입
+            val_issue = i.get("주요사항", "")
+            worksheet.merge_range(curr_row, 1, curr_row, 2, str(val_issue), cell_fmt)
+            # 3열 비고 기입
             worksheet.write(curr_row, 3, i.get("비고", ""), cell_fmt)
             curr_row += 1
         curr_row += 1
 
         # 4. 사업진행현황 섹션
-        worksheet.merge_range(curr_row, 0, curr_row, 2, " # 사업진행현황", title_fmt)
+        worksheet.merge_range(curr_row, 0, curr_row, 3, " # 사업진행현황", title_fmt) # 0~3까지 제목 병합
         curr_row += 1
-        headers = ["일자", "주요사항", "비고"]
-        for col, h in enumerate(headers):
-            worksheet.write(curr_row, col, h, head_fmt)
+
+        # 헤더 병합 (일자:0, 주요사항:1~2합병, 비고:3)
+        worksheet.write(curr_row, 0, "일자", head_fmt)
+        worksheet.merge_range(curr_row, 1, curr_row, 2, "주요사항", head_fmt)
+        worksheet.write(curr_row, 3, "비고", head_fmt)
         curr_row += 1
+
         for s in data.get('status', []):
             worksheet.write(curr_row, 0, s.get("일자", ""), cell_fmt)
-            
-            if "주요사항" in basic_data:
-                worksheet.merge_range(curr_row, 1, curr_row, 3, str(basic_data["주요사항"]), cell_fmt)
-                worksheet.write(curr_row, 1, s.get("주요사항", ""), cell_fmt)
-                basic_data.pop("주요사항")
-
+            # 1~2열 병합하여 주요사항 기입
+            val_status = s.get("주요사항", "")
+            worksheet.merge_range(curr_row, 1, curr_row, 2, str(val_status), cell_fmt)
+            # 3열 비고 기입
             worksheet.write(curr_row, 3, s.get("비고", ""), cell_fmt)
             curr_row += 1
 
